1b014cad80419197aafd8c062cedd1de29c1c9e9
I478554 <t.rangelov@student.fontys.nl>
2023-05-15 13:43:21 +0200

    extracted client secret into env variable

--- infralab-authentication-service-template/src/main/java/fhict/nl/infralabauthenticationservice/controllers/AuthenticationController.java
+++ infralab-authentication-service-template/src/main/java/fhict/nl/infralabauthenticationservice/controllers/AuthenticationController.java
@@ -2,8 +2,9 @@ package fhict.nl.infralabauthenticationservice.controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.*;
-import org.springframework.security.oauth2.client.endpoint.WebClientReactiveAuthorizationCodeTokenResponseClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,33 +21,32 @@ import java.io.IOException;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class AuthenticationController {
+    @Autowired
+    Environment env;
+
 
     @GetMapping
     public ResponseEntity<String> returnResponse(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
-        //TODO: we exchange the authorization code(request param) for an access token (server-side) right here.
-        //TODO: afterwards, we redirect back to our front end where we show the certificate.
-        //TODO: we can add a protected route to the front-end where we show the certificate and check for the access token every time we redirect there
         String tokenEndpoint = "https://identity.fhict.nl/connect/token";
         WebClient client = WebClient.create();
         MultiValueMap<String, String> bodyValues = new LinkedMultiValueMap<>();
-        // this is really fucking bad but for now im leaving it here cuz im tired,
+        //this is really fucking bad but for now im leaving it here cuz im tired,
         //gonna divide the logic into business classes next time, but this works and gets the token - Tsvetislav
         bodyValues.add("grant_type", "authorization_code");
         bodyValues.add("code", code);
         bodyValues.add("redirect_uri", "https://localhost:8080/");
         bodyValues.add("client_id", "i476232-infralabau");
-        //god forgive me for i have sinned please whoever does this after me put this in an environment variable - Tsvetislav
-        bodyValues.add("client_secret", "Kfr2y1VUMSDESVoSaD2z2iOuVn1dzPr6Zs8wj8G2");
+        bodyValues.add("client_secret",env.getProperty("client_secret"));
 
 
         String res = client.post()
-                        .uri(tokenEndpoint)
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                                        .accept(MediaType.APPLICATION_JSON)
-                                                .body(BodyInserters.fromFormData(bodyValues))
-                                                        .retrieve()
-                                                                .bodyToMono(String.class)
-                                                                        .block();
+                .uri(tokenEndpoint)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromFormData(bodyValues))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
 
         response.addHeader("authorization", "ACCESS_TOKEN_GOES_HERE");
@@ -54,5 +54,4 @@ public class AuthenticationController {
         response.sendRedirect("http://localhost:3000/");
         return ResponseEntity.ok(null);
     }
-
 }
